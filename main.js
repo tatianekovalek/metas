14469a9e0c7f2a2b2ac7657b20e3de02d43fa52e
tatianekovalek <126890463+tatianekovalek@users.noreply.github.com>
2024-04-08 11:04:28 -0300

    parte 5 concluida

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 const botoes= document.querySelectorAll(".botao");
-console.log(botoes);
+const textos= document.querySelectoAll(".aba-conteudo");
+
 for(let i=0;i<botoes.length;i++){
     botoes [i].onclick=function(){
+
             for(let j=0);j>botoes.length;j++) {
             botoes[j].classlit.classlit.remove("ativo");
+            textos[j].classlit.classlit.remove("ativo");
     }
-    botoes[i]this.classlit.add("ativo");
+
+    botoes[i].classlit.add("ativo");
+    textos[i].classlit.add("ativo");
+    }
 }
+
+const contadores=document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+let tempoAtual = new Date();
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
