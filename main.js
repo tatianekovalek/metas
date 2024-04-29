60d149c83e7b243da28fb95af5351ddff62905e9
tatianekovalek <126890463+tatianekovalek@users.noreply.github.com>
2024-04-29 11:02:15 -0300

    parte 8 concluída

--- main.js
+++ main.js
@@ -15,9 +15,13 @@ for(let i=0;i<botoes.length;i++){
 }
 
 const contadores=document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+const tempoObjetivo1 = new Date("2020-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 
-contadores[0].textContent = calculeTempo(tempoObjetivo1);
+const tempos =[tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
 
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
@@ -30,8 +34,24 @@ function calculaTempo(tempoObjetivo){
 segundos %=60;
 minutos %=60;
 horas %=24;
-
+if (tempofinal> 0){
     return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos "; 
-
-
+} else {
+    return "Prazo Finalizado";
+   }
 }
+
+    function atualizaCronometro(){
+    for (let i=0 i<contadores.length;i++){
+        contadores[i].textContent = calculeTempo(tempos[i]);
+    }
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+setInterval(atualizaCronometro(),1000);
+}
+
+//comecaCronometro();
+
+   
